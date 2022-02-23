5d130d3150377f362baa185846a9262137433196
dargentieri <94847570+dargentieri@users.noreply.github.com>
2022-02-23 18:12:44 +0100

    create json

--- script.js
+++ script.js
@@ -35,29 +35,44 @@ dropArea.addEventListener("drop", (event)=>{
 });
 
 function showFile(){
+  let intestazione=[];
+  let stampa="[";
   var fileUpload = document.getElementById("fileUpload");
   var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.csv|.txt)$/;
   if (regex.test(fileUpload.value.toLowerCase())) {
       if (typeof (FileReader) != "undefined") {
           var reader = new FileReader();
           reader.onload = function (e) {
-              let stampa="";
-              var table = document.createElement("table");
               var rows = e.target.result.split("\n");
               for (var i = 0; i < rows.length; i++) {
                   var cells = rows[i].split(",");
                   if (cells.length > 1) {
-                      var row = table.insertRow(-1);
+                      if(i!=0)
+                        stampa= stampa+'{';
                       for (var j = 0; j < cells.length; j++) {
-                          var cell = row.insertCell(-1);
-                          cell.innerHTML = cells[j];
-                          stampa += cells[j];
+                        if(i==0)
+                        {
+                          intestazione[j]=cells[j];
+                        }
+                        else
+                        {
+                            if(j==cells.length-1)
+                              stampa=stampa+'"'+intestazione[j]+'":"'+cells[j]+'"';
+                            else
+                            stampa=stampa+'"'+intestazione[j]+'":"'+cells[j]+'",';
+                        }
                       }
+                      if(i!=0)
+                      {
+                        if(i==rows.length-1)
+                        stampa=stampa+'}';
+                        else 
+                          stampa=stampa+'},';
+                      }
+
                   }
               }
-              var dvCSV = document.getElementById("dvCSV");
-              dvCSV.innerHTML = "";
-              dvCSV.appendChild(table);
+              stampa=stampa+']';
               console.log(stampa);
           }
           reader.readAsText(fileUpload.files[0]);
